e48e6410206c3d3711535ce23b80625f95dfb848
Ader <Ader@gmail.com>
2020-08-04 23:33:44 +0800

    add

---
+++ src/routes/member.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/login', (req, res)=>{
+
+    res.render('member/login');
+});
+
+router.post('/login', (req, res)=>{
+    console.log(req.body);
+    const users = {
+        'shinder': {
+           nickname: '小明',
+           pw: '23456'
+        },
+        'hello': {
+            nickname: '是在哈囉',
+            pw: '123456'
+        },
+    };
+    const output = {
+        success: false,
+        error: '帳號或密碼錯誤',
+        body: req.body
+    };
+
+    if(req.body.account && users[req.body.account] ){
+        // 帳號是對的
+        if(req.body.password===users[req.body.account].pw){
+            // 密碼也是對的
+            req.session.loginUser = {
+                account: req.body.account,
+                nickname: users[req.body.account].nickname
+            };
+            output.success = true;
+            delete output.error;
+        }
+    }
+
+    res.json(output);
+});
+
+
+router.get('/logout', (req, res) => {
+    delete req.session.loginUser;
+    res.redirect('/');
+})
+
+
+module.exports = router;
